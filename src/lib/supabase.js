aaf791e5aaaf844cd7beea59da216d84b4bbf899
ghsam1977 <wissam.ghaddar@gmail.com>
2026-06-07 15:59:48 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-const URL = import.meta.env.VITE_SUPABASE_URL || ''
-const KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || ''
+const URL = import.meta.env.VITE_SUPABASE_URL || 'https://dourrjhhkkkfvjsiqvwu.supabase.co'
+const KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_Gibpk1thF40NAxb7qnYRhQ_n2YtxJeS'
 export const supabase = createClient(URL, KEY)
